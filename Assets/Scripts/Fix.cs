b1667dc297fcf5ab63ec39fe118145430b4e73ef
Oleg Morozov <benjminmoore@gmail.com>
2020-06-23 18:00:26 +0300

    Initialize

---
+++ Assets/Scripts/Fix.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+
+public class Fix : MonoBehaviour {
+    private List<TMP_Text> list;
+
+    private void Awake() {
+        var manager = TMP_UpdateManager.instance;
+        this.list = (List<TMP_Text>) manager.GetType()
+            .GetField("m_InternalUpdateQueue", BindingFlags.Instance | BindingFlags.NonPublic)
+            .GetValue(manager);
+    }
+
+    private void Update() {
+        this.list.Clear();
+    }
+}
---
+++ Assets/Scripts/Issue.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+//Update any UI element to force canvas rebuild, it can be any element: text, image, slider, etc.
+//It will trigger TMP_UpdateManager.OnCameraPreCull -> TMP_UpdateManager.DoRebuilds -> TextMeshProUGUI.InternalUpdate().
+//A very expensive operation will be called for each text, even if it has not changed -> Transform.get_lossyScale().
+//On low-end devices this kills any performance.
+//In the example, 4000 instances of text to show a lag on a PC, but on real mobile projects 20-100 instances are enough.
+//
+//In Fix.cs is fix which we have to use in production.
+//Just enable it on the Issue object in the SampleScene to see the difference.
+public class Issue : MonoBehaviour {
+    public TextMeshProUGUI text;
+    
+    private void Update() {
+        this.text.text = Time.time.ToString(CultureInfo.InvariantCulture);
+    }
+}
